eb58bda7792ce85fd92c33d27a7cac29d3a143c6
Yingge(Freddie) He <heyingge@uw.edu>
2021-06-10 13:35:45 -0700

    Add files via upload

---
+++ main_atari.py
@@ -0,0 +1,34 @@
+import functools
+import gym
+from Config import Config
+from util import train_pixel
+from Models import ActorCritic
+from Networks import cnn_head_model, actor_model, critic_model, head_model
+from Memory import Memory
+import wandb
+
+wandb.init(project="PPO")
+import matplotlib.pyplot as plt
+import numpy as np
+
+env_id = "BreakoutNoFrameskip-v4"
+config = Config(env_id)
+
+config.update_every = 128
+config.num_learn = 8
+config.win_condition = 230
+config.n_steps = 5e7
+config.hidden_size = 512
+config.lr = 1e-4
+config.lr_annealing = True
+config.epsilon_annealing = True
+
+config.memory = Memory
+config.model = ActorCritic
+config.head_model = functools.partial(cnn_head_model, config)
+config.actor_model = functools.partial(actor_model, config)
+config.critic_model = functools.partial(critic_model, config)
+
+config.init_wandb()
+
+scores = train_pixel(config)
